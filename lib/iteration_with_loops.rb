fa1a7b2a69c42fe7c6792a97e682f0469f217f56
adventurous-format-6947 <bminczu@gmail.com>
2020-10-07 16:19:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ else
 end
 element_count+=1
 end
-endindex+=1
+index+=1
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
   #
   # Output all even values in each nested array
